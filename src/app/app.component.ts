71884848aca9d1a70b407059053c72b4e1c7fa51
NikRokzz <nikhil.malewar@d2kindia.com>
2018-08-06 18:23:10 +0530

    Added bootstrap and fontawesome to project.
    Added web page design css for main page. (close icon, side nav, navbar, main content).
    Added web page content of index home page .
    Changes in appcomponent ts file for transition and drag for footer element.
    Added webpage backgroung image.
    Added custom spacing file for margin padding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  openNav(){
+    document.getElementById('side-menu__container').style.width = '270px';
+    document.getElementById('main__container').style.marginLeft = '270px';
+    document.getElementById('main__header').style.marginLeft = '270px';
+    document.getElementById('main__footer').style.marginLeft = '270px';
+    document.body.style.backgroundColor = 'rgba(0,0,0,0.4)';
+  }
+
+  closeNav() {
+    document.getElementById('side-menu__container').style.width = '0';
+    document.getElementById('main__container').style.marginLeft = '0';
+    document.getElementById('main__header').style.marginLeft = '0';
+    document.getElementById('main__footer').style.marginLeft = '0';
+    document.body.style.backgroundColor = 'white';
+  }
+
 }
